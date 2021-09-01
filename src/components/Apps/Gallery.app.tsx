2229ed02a0d7a96a4739f5361fb2a6d709733d7e
amirhosein ahmadi <stranger.ahmadi@gmail.com>
2021-09-01 08:17:19 +0430

    refactore on browser app , adding gallery app

--- src/components/Apps/Gallery.app.tsx
+++ src/components/Apps/Gallery.app.tsx
@@ -1,7 +1,37 @@
 import React, { FC } from "react";
+import { useQuery } from "react-query";
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+  accessKey: "qje0zQPWbuwxieNSDAY1qulJ5kr6Jq34PNSRLaBjGiQ",
+});
 
 const Gallery: FC = () => {
-  return <div>Gallery</div>;
+  const { isLoading, data } = useQuery<any>("Gallery", () =>
+    unsplash.search
+      .getPhotos({ query: "wanderlust", perPage: 30, page: 1 })
+      .then((res) => {
+        return res.response?.results;
+      })
+  );
+  console.log(data);
+  return (
+    <div
+      style={{ columns: " 6 200px" }}
+      className="w-full h-full p-3 overflow-auto"
+    >
+      {data &&
+        !isLoading &&
+        data.map((im: any) => (
+          <img
+            key={im.id}
+            className="mb-3 rounded-lg"
+            src={im.urls.full}
+            alt=""
+          />
+        ))}
+    </div>
+  );
 };
 
 export default Gallery;
